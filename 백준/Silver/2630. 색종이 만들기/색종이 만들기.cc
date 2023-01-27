7c30af37b580505a4dd2c3ce3290b41817bc85d2
Famec <68315380+Famec7@users.noreply.github.com>
2023-01-27 21:30:44 +0900

    [Silver II] Title: 색종이 만들기, Time: 4 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int N;
+int blue, white;
+int board[128][128];
+
+void check(int y, int x, int size);
+
+int main() {
+  std::cin >> N;
+
+  for (int y = 0; y < N; y++)
+    for (int x = 0; x < N; x++)
+      std::cin >> board[y][x];
+
+  check(0, 0, N);
+  std::cout << white << '\n' << blue << '\n';
+
+  return 0;
+}
+
+void check(int y, int x, int size) {
+  int color = board[y][x];
+  for (int dy = y; dy < y + size; dy++) {
+    for (int dx = x; dx < x + size; dx++) {
+      if (color != board[dy][dx]) {
+        color = 2;
+      }
+      if (color == 2) {
+        check(y, x, size / 2);
+        check(y, x + size / 2, size / 2);
+        check(y + size / 2, x, size / 2);
+        check(y + size / 2, x + size / 2, size / 2);
+        return;
+      }
+    }
+  }
+  if (color == 0)
+    white++;
+  else
+    blue++;
+}
