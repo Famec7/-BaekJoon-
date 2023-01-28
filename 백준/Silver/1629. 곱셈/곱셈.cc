fa64e9d241f5c4e39fd6250f0952fbb033b80a02
Famec <68315380+Famec7@users.noreply.github.com>
2023-01-28 21:25:54 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -11,13 +11,12 @@ int main() {
 }
 
 long long power(long long b) {
-  if (b == 0)
-    return 1;
   if (b == 1)
     return A % C;
 
   long long half = power(b / 2) % C;
   if (b % 2 == 0)
     return half * half % C;
-  return half * half % C * A % C;
+  else
+    return half * half % C * A % C;
 }
