6b0499cd36fe68ca4953e6edd0bad06477a6a2cb
Famec <68315380+Famec7@users.noreply.github.com>
2023-02-01 20:20:26 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  int N, x;
+  std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+
+  std::cin >> N;
+  while (N--) {
+    std::cin >> x;
+    if (x == 0) {
+      if (pq.empty())
+        std::cout << '0' << '\n';
+      else {
+        std::cout << pq.top() << '\n';
+        pq.pop();
+      }
+    } else
+      pq.push(x);
+  }
+
+  return 0;
+}
