ef7ef2b52818a6521bff2f47926471ffbee3d4e5
Famec <68315380+Famec7@users.noreply.github.com>
2023-02-26 21:48:54 +0900

    [Gold V] Title: 다각형의 면적, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
@@ -1,27 +1,27 @@
-#include <cmath>
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 int N;
-double result;
-std::vector<std::pair<long long, long long>> polygon;
+std::vector<std::pair<double, double>> polygon;
 
-long long outer_product(long long x1, long long y1, long long x2, long long y2);
+double outer_product(double x1, double y1, double x2, double y2);
 
 int main() {
   std::cin >> N;
 
-  long long x, y;
+  double x, y;
   for (int i = 0; i < N; i++) {
     std::cin >> x >> y;
     polygon.push_back({x, y});
   }
+  double result = 0;
   for (int i = 1; i < N - 1; i++) {
-    long long x1 = polygon[i].first - polygon[0].first;
-    long long y1 = polygon[i].second - polygon[0].second;
-    long long x2 = polygon[i + 1].first - polygon[0].first;
-    long long y2 = polygon[i + 1].second - polygon[0].second;
-    result += (double)(outer_product(x1, y1, x2, y2) / 2.0);
+    double x1 = polygon[i].first - polygon[0].first;
+    double y1 = polygon[i].second - polygon[0].second;
+    double x2 = polygon[i + 1].first - polygon[0].first;
+    double y2 = polygon[i + 1].second - polygon[0].second;
+    result += outer_product(x1, y1, x2, y2) / 2.0;
   }
   std::cout << std::fixed;
   std::cout.precision(1);
@@ -30,7 +30,6 @@ int main() {
   return 0;
 }
 
-long long outer_product(long long x1, long long y1, long long x2,
-                        long long y2) {
+double outer_product(double x1, double y1, double x2, double y2) {
   return x1 * y2 - x2 * y1;
 }
