83886d3a5566360e7c5cfd864343843307e8451f
Famec <68315380+Famec7@users.noreply.github.com>
2024-01-22 19:45:45 +0900

    [Gold III] Title: 내리막 길, Time: 100 ms, Memory: 3852 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -1,43 +1,60 @@
+#include <functional>
 #include <iostream>
+#include <queue>
 
-const int INF = 987654321;
+typedef struct _Point {
+  int height;
+  int x;
+  int y;
+} Point;
+
+struct comp {
+  bool operator()(Point &a, Point &b) { return a.height < b.height; }
+};
 
 int N, M;
-int board[500][500], cache[501][501];
-int dx[4] = {0, 0, 1, -1};
-int dy[4] = {1, -1, 0, 0};
+int map[500][500];
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
 
-int solve(int y, int x);
+int solve(int x, int y) {
+  int route[500][500] = {0};
+  std::priority_queue<Point, std::vector<Point>, comp> container;
+  container.push({map[y][x], x, y});
+  route[y][x] = 1;
+
+  while (not container.empty()) {
+    int cur_x = container.top().x;
+    int cur_y = container.top().y;
+    container.pop();
+
+    for (int i = 0; i < 4; i++) {
+      int nx = cur_x + dx[i];
+      int ny = cur_y + dy[i];
+
+      if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+        continue;
+      if (map[ny][nx] < map[cur_y][cur_x]) {
+        if (route[ny][nx] == 0)
+          container.push({map[ny][nx], nx, ny});
+        route[ny][nx] += route[cur_y][cur_x];
+      }
+    }
+  }
+
+  return route[M - 1][N - 1];
+}
 
 int main() {
   std::cin >> M >> N;
 
-  std::fill(&cache[0][0], &cache[500][501], -1);
-  for (int y = 0; y < M; y++)
+  for (int y = 0; y < M; y++) {
     for (int x = 0; x < N; x++)
-      std::cin >> board[y][x];
-
-  std::cout << solve(0, 0) << '\n';
-
-  return 0;
-}
-
-int solve(int y, int x) {
-  if (y == M - 1 && x == N - 1)
-    return 1;
-
-  int &ret = cache[y][x];
-  if (ret != -1)
-    return ret;
-
-  ret = 0;
-  for (int i = 0; i < 4; i++) {
-    int ny = y + dy[i];
-    int nx = x + dx[i];
-    if (ny >= 0 && nx >= 0 && ny < M && nx < N)
-      if (board[y][x] > board[ny][nx])
-        ret += solve(ny, nx);
+      std::cin >> map[y][x];
   }
 
-  return ret;
+  int result = solve(0, 0);
+  std::cout << result << '\n';
+
+  return 0;
 }
